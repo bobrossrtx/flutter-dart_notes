bb92620123c32c282355b5b86a0f7e26d1fe7911
bobrossrtx <73446766+bobrossrtx@users.noreply.github.com>
2020-12-06 19:33:45 +0000

    Create notes.dart

---
+++ notes.dart
@@ -0,0 +1,73 @@
+// integer
+// int age = 27;
+// print(age);
+
+//+++++++++++++++++++++++++++++++++++++++++//
+
+// string
+// String name = 'dennis samsung';
+// print(name);
+
+//+++++++++++++++++++++++++++++++++++++++++//
+
+// You Can Change a string with another string,      but you cant change its type.
+// name = 'ken';
+// print(name);
+
+//+++++++++++++++++++++++++++++++++++++++++//
+
+// bool / boolean
+// bool isNight = true;
+// print(isNight);
+
+//+++++++++++++++++++++++++++++++++++++++++//
+
+//  A dynamic type can be changed.
+//  dynamic name = 'chung-li';
+//  name = 30;
+//  print(name);
+
+//+++++++++++++++++++++++++++++++++++++++++//
+
+//  Main function of a dart file.
+//  void main() {
+
+//  }
+
+//+++++++++++++++++++++++++++++++++++++++++//
+
+//  How to return different types within a           function
+//  void main() {
+//    print('something');
+//    String greet = greeting();
+//    int age = getAge();
+//    print(age);
+//    print(greet);
+//  }
+
+//  String greeting() {  |or|   String greeting() => 'hello';
+//    return 'hello';    |or|
+//  }                    |or|
+
+//  int getAge() {       |or|   int getAge() => 28;
+//    return 28;         |or|
+//  }                    |or|
+
+//+++++++++++++++++++++++++++++++++++++++++//
+
+//  List names = ['chun-li', 'mario', 'ken'];
+//  print(names);
+
+//+++++++++++++++++++++++++++++++++++++++++//
+
+//  List names = ['chun-li', 'mario', 'ken'];
+  
+//    names.add('luigi');
+//    names.remove('ken');
+
+//  You can use any data type in a list. (not recomended)
+//  Eg: an integer
+//    names.add(30);
+  
+//    print(names);
+
